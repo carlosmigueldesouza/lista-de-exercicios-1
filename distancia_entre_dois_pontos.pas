20dc555be1606db0794e8f882b5ff1408f81ea07
CARLOS MIGUEL DE SOUZA <carlossouza2304@gmail.com>
2025-03-17 19:35:09 -0300

    Create distancia_entre_dois_pontos.pas

---
+++ distancia_entre_dois_pontos.pas
@@ -0,0 +1,21 @@
+Program distancia_de_dois_pontos ;
+	var x, y, x1, y1, dist:real;
+Begin
+	writeln('me informe os pontos x e y das cordenadas');
+	readln(x,y);
+	writeln('me informe mais dois pontos');
+	readln(x1,y1);
+	if(x1=x) then 
+	begin
+	dist:=(y-y1);
+	write('a distancia entre os pontos y é' , dist:6:2);
+	end 
+	else
+	if (x <> x1)  then
+	begin                                   
+	dist:=(((x-x1) * (x-x1)) + ((y-y1)* (y-y1)));  //faltou radiciação
+	write('A distancia entre os dois pontos é' , dist:6:2);
+	end 
+	else  ;
+	readkey;                      
+end.
