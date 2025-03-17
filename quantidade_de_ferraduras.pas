f70f67036a85b5338d90a4a79433394ea0e25efc
CARLOS MIGUEL DE SOUZA <carlossouza2304@gmail.com>
2025-03-17 19:25:00 -0300

    Create quantidade_de_ferraduras.pas

---
+++ quantidade_de_ferraduras.pas
@@ -0,0 +1,8 @@
+Program quantidade_de_ferradura ;
+var cavalos, ferraduras:real;
+Begin
+writeln('Me informe a quantidade de cavalos no aras:');
+readln(cavalos);
+ferraduras:=(cavalos*4);
+writeln('a quantidade de ferraduras é,' , ferraduras:0:0);  
+End.
