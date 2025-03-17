50202eb467c1c5706ea91dc0d51a2126c0437276
CARLOS MIGUEL DE SOUZA <carlossouza2304@gmail.com>
2025-03-17 19:24:22 -0300

    Create area_retangulo.pas

---
+++ area_retangulo.pas
@@ -0,0 +1,10 @@
+Program area_terreno ;
+var altura, base, area:real;
+Begin
+writeln('me informe uma das dimensões do terreno');
+readln(base);
+writeln('me informe a outra dimenssão');
+readln(altura);
+area:=(base*altura);
+writeln('a area do terreno é, ', area:0:2);
+End.
