67458801ccc0a0222f5a892b25d3d23228cc8c22
CARLOS MIGUEL DE SOUZA <carlossouza2304@gmail.com>
2025-03-17 19:27:13 -0300

    Create dias_vividos.pas

---
+++ dias_vividos.pas
@@ -0,0 +1,11 @@
+Program dias_vividos;
+var nome:string;
+var idade, dias:real;
+Begin
+write('escreva seu nome:');
+readln(nome);
+write('escreva sua idade');
+readln(idade);
+dias:=(365*idade);
+writeln(nome , ' você ja viveu ' , dias:0:0 , ' dias');    
+End.
